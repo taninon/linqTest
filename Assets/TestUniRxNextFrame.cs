253a6c8af6218b1ac9d9b80d50c7113adcadef10
noriaki_tanimichi <tanimichi@morality-line.com>
2020-06-23 08:19:57 +0900

    TestUniRxNextFrame

---
+++ Assets/TestUniRxNextFrame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class TestUniRxNextFrame : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Observable.NextFrame().Subscribe(_ =>
+        {
+            Debug.Log("1フレーム後に実行");
+        }).AddTo(this);
+
+        Debug.Log("すぐ実行");
+    }
+
+}
