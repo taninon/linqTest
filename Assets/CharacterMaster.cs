3cb9a051d9b759b2af4589ab229c73bf37603ba8
noriaki_tanimichi <tanimichi@morality-line.com>
2020-06-23 02:12:28 +0900

    SctiptableObject

---
+++ Assets/CharacterMaster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/CreateAsset")]
+public class CharacterMaster : ScriptableObject
+{
+    public List<CharacterMasterRecord> sheet;
+
+    public enum JobPost
+    {
+        Programer,
+        Modeler,
+        Motion,
+        Director
+    }
+
+    public enum Rarity
+    {
+        N = 0,
+        R = 1,
+        SR = 2,
+        SSR = 3
+    }
+
+
+    [Serializable]
+    public class CharacterMasterRecord
+    {
+        public string Name;
+        public int Hp;
+        public CharacterMaster.Rarity Rarity;
+        public CharacterMaster.JobPost Post;
+    }
+}
---
+++ Assets/TestLinq.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TestLinq : MonoBehaviour
+{
+    [SerializeField] CharacterMaster data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var SR = data.sheet
+            .OrderBy(r => r.Hp);
+
+        foreach (var c in SR) {
+            Debug.Log(c.Name);
+        }
+
+        var list = new List<int> { 1, 84, 95, 95, 40, 6 };
+
+            Debug.Log(list.All(x => x > 0));
+        Debug.Log(list.All(x => x > 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
