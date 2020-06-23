537875699ec79b72c8b525cbe4195bf486dc9328
noriaki_tanimichi <tanimichi@morality-line.com>
2020-06-23 10:55:57 +0900

    Throtttle

---
+++ Assets/TestUniRxThrottle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class TestUniRxThrottle : MonoBehaviour
+{
+    Subject<string> attackSubject = new Subject<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackSubject
+            .ThrottleFirst(TimeSpan.FromSeconds(2))
+            .Subscribe(text => Debug.Log("イベント発生:" + text));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackSubject.OnNext("攻撃をうけた");
+
+    }
+}
