b2d6b4366242518ed9ee173b158bd8a3ba418970
noriaki_tanimichi <tanimichi@morality-line.com>
2020-06-23 10:45:39 +0900

    イベントを購読する

---
+++ Assets/TestUniRxSubject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class TestUniRxSubject : MonoBehaviour
+{
+    Subject<string> attackSubject = new Subject<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackSubject.Subscribe(text => Debug.Log("イベント発生:" + text));
+
+        attackSubject.OnNext("攻撃をうけた");
+        attackSubject.OnNext("呪文をうけた");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
