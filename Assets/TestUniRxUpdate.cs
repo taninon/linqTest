44643943e30d7ead4f884577e3ec9be4950b9c8c
noriaki_tanimichi <tanimichi@morality-line.com>
2020-06-23 10:11:00 +0900

    UniRxUpdate

---
+++ Assets/TestUniRxUpdate.cs
@@ -0,0 +1,30 @@
+﻿using UniRx.Triggers;
+using UniRx;
+using UnityEngine;
+
+public class TestUniRxUpdate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        this.UpdateAsObservable()
+        .Subscribe(x => Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))));
+
+        this.UpdateAsObservable()
+        .Where(_ => Input.GetKeyDown(KeyCode.Space))
+        .Subscribe(_ => Jump());
+
+    }
+
+    private void Move(Vector3 set)
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+
+}
